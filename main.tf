ee23732c0fe1ed97ab13041a25864dc5f347c104
prathjan <prathjan@cisco.com>
2021-04-25 16:24:10 -0700

    Refer to globalvars WS for vars

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+#get the data fro the global vars WS
+data "terraform_remote_state" "global" {
+  backend = "remote"
+  config = {
+    organization = "CiscoDevNet"
+    workspaces = {
+      name = var.globalwsname
+    }
+  }
+}
+
 terraform {
   required_providers {
     intersight = {
@@ -13,32 +24,27 @@ provider "intersight" {
   endpoint = "https://intersight.com"
 }
 
-variable "cluster_name" {
-  type        = string
-  description = "Cluster name"
-}
-
 module "infra_config_policy" {
   source           = "terraform-cisco-modules/iks/intersight//modules/infra_config_policy"
-  name             = "${var.cluster_name}-infra-config"
-  device_name      = var.device_name
-  vc_portgroup     = var.portgroup
-  vc_datastore     = var.datastore
-  vc_cluster       = var.cluster
-  vc_resource_pool = var.resource_pool
-  vc_password      = var.password
-  org_name         = var.organization
+  name             = local.infra_config_policy 
+  device_name      = local.device_name
+  vc_portgroup     = local.portgroup
+  vc_datastore     = local.datastore
+  vc_cluster       = local.cluster
+  vc_resource_pool = local.resource_pool
+  vc_password      = local.password
+  org_name         = local.organization
 #  tags             = var.tags
 }
 
 module "ip_pool_policy" {
   source           = "terraform-cisco-modules/iks/intersight//modules/ip_pool"
-  name             = "${var.cluster_name}-ip-pool"
-  starting_address = var.starting_address
-  pool_size        = var.pool_size
-  netmask          = var.netmask
-  gateway          = var.gateway
-  primary_dns      = var.primary_dns
+  name             = local.ip_pool_policy 
+  starting_address = local.starting_address
+  pool_size        = local.pool_size
+  netmask          = local.netmask
+  gateway          = local.gateway
+  primary_dns      = local.primary_dns
 #  secondary_dns    = var.ip_secondary_dns
 
   org_name = var.organization
@@ -47,34 +53,89 @@ module "ip_pool_policy" {
 
 module "network" {
   source      = "terraform-cisco-modules/iks/intersight//modules/k8s_network"
-  policy_name = var.cluster_name
-  dns_servers = [var.primary_dns]
-  ntp_servers = [var.primary_dns]
-  timezone    = var.timezone
-  domain_name = var.domain_name
-  org_name    = var.organization
+  policy_name = local.clustername
+  dns_servers = [local.primary_dns]
+  ntp_servers = [local.primary_dns]
+  timezone    = local.timezone
+  domain_name = local.domain_name
+  org_name    = local.organization
 #  tags        = var.tags
 }
 
 module "k8s_version" {
   source           = "terraform-cisco-modules/iks/intersight//modules/version"
-  k8s_version      = "1.19.5"
-  k8s_version_name = "${var.cluster_name}-1.19.5"
+  k8s_version      = local.k8s_version 
+  k8s_version_name = local.k8s_version_name 
 
-  org_name = var.organization
+  org_name = local.organization
 #  tags     = var.tags
 }
 
 data "intersight_organization_organization" "organization" {
-  name = var.org_name
+  name = local.organization
 }
 resource "intersight_kubernetes_virtual_machine_instance_type" "masterinstance" {
-  name      = "${var.cluster_name}-master-instance"
-  cpu       = var.cpu
-  disk_size = var.disk_size
-  memory    = var.memory
+  name      = local.masterinstance
+  cpu       = local.cpu
+  disk_size = local.disk_size
+  memory    = local.memory
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.organization.results.0.moid
   }
 }
+
+
+
+locals {
+  masterinstance = yamldecode(data.terraform_remote_state.global.outputs.masterinstance)
+  cpu = yamldecode(data.terraform_remote_state.global.outputs.cpu)
+  disk_size = yamldecode(data.terraform_remote_state.global.outputs.disk_size)
+  memory = yamldecode(data.terraform_remote_state.global.outputs.memory)
+  organization= yamldecode(data.terraform_remote_state.global.outputs.organization)
+  k8s_version = yamldecode(data.terraform_remote_state.global.outputs.k8s_version)
+  k8s_version_name = yamldecode(data.terraform_remote_state.global.outputs.k8s_version_name)
+  clustername = yamldecode(data.terraform_remote_state.global.outputs.clustername)
+  primary_dns = yamldecode(data.terraform_remote_state.global.outputs.primary_dns)
+  timezone = yamldecode(data.terraform_remote_state.global.outputs.timezone)
+  domain_name = yamldecode(data.terraform_remote_state.global.outputs.domain_name)
+  ip_pool_policy = yamldecode(data.terraform_remote_state.global.outputs.ip_pool_policy)
+  starting_address = yamldecode(data.terraform_remote_state.global.outputs.starting_address)
+  pool_size = yamldecode(data.terraform_remote_state.global.outputs.pool_size)
+  netmask = yamldecode(data.terraform_remote_state.global.outputs.netmask)
+  gateway = yamldecode(data.terraform_remote_state.global.outputs.gateway)
+  infra_config_policy = yamldecode(data.terraform_remote_state.global.outputs.infra_config_policy)
+  device_name = yamldecode(data.terraform_remote_state.global.outputs.device_name)
+  portgroup = yamldecode(data.terraform_remote_state.global.outputs.portgroup)
+  datastore = yamldecode(data.terraform_remote_state.global.outputs.datastore)
+  vspherecluster = yamldecode(data.terraform_remote_state.global.outputs.vspherecluster)
+  resource_pool = yamldecode(data.terraform_remote_state.global.outputs.resource_pool)
+  password = yamldecode(data.terraform_remote_state.global.outputs.password)
+  ip_pool_policy = yamldecode(data.terraform_remote_state.global.outputs.ip_pool_policy)
+  starting_address = yamldecode(data.terraform_remote_state.global.outputs.starting_address)
+  pool_size = yamldecode(data.terraform_remote_state.global.outputs.pool_size)
+  netmask = yamldecode(data.terraform_remote_state.global.outputs.netmask)
+  gateway = yamldecode(data.terraform_remote_state.global.outputs.gateway)
+  primary_dns = yamldecode(data.terraform_remote_state.global.outputs.primary_dns)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -1,33 +1,33 @@
-api_key_id = ""
-api_private_key = ""
-
-org_name = "default"
+#api_key_id = ""
+#api_private_key = ""
+globalwsname = "globalvar"
+#org_name = "default"
 
 #infra_config_policy params
-cluster_name = "testsjc2" 
-device_name = "10.200.0.210"
-portgroup = ["Management"]
-datastore = "hx-demo-ds1"
-cluster = "hx-demo"
-resource_pool = "Test_Resource_Pool"
-password = ""
-organization = "default"
+#cluster_name = "testsjc2" 
+#device_name = "10.200.0.210"
+#portgroup = ["Management"]
+#datastore = "hx-demo-ds1"
+#cluster = "hx-demo"
+#resource_pool = "Test_Resource_Pool"
+#password = ""
+#organization = "default"
 
 #ip_pool_policy params
-starting_address = "10.200.0.20"
-pool_size = "30"
-netmask = "255.255.255.0"
-gateway = "10.200.0.254"
+#starting_address = "10.200.0.20"
+#pool_size = "30"
+#netmask = "255.255.255.0"
+#gateway = "10.200.0.254"
 primary_dns = "10.200.0.100"
 
 #network params
-timezone = "America/New_York"
-domain_name = "demo.intra"
+#timezone = "America/New_York"
+#domain_name = "demo.intra"
 
 #k8s_version
-k8s_version = "1.19.5"
+#k8s_version = "1.19.5"
 
 #instance type
-cpu = "4"
-disk_size = "40"
-memory = "16384"
+#cpu = "4"
+#disk_size = "40"
+#memory = "16384"
